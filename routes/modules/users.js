626f7dc4691d30aaa1a09d9950ec0ffd6f45937f
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-01-22 22:44:35 +0100

    Modify register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
-
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -19,8 +19,31 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      // errors.push({ message: '此 Email 已註冊。' })
+      return res.render('register', {
+        // errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+      .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash // 用雜湊值取代原本的使用者密碼
+      })
+      )
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+
+  })
 })
 
 router.get('/logout', (req, res) => {
