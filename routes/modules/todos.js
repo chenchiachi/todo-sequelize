61376c68fd48d102f18954dc7353551f4b054e94
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-01-23 13:50:58 +0100

    Add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -10,8 +10,8 @@ router.get('/new', (req, res) => {
 router.post('/', (req, res) => {
   const UserId = req.user.id
   const name = req.body.name
-  return Todo.create({ name, UserId }) 
-    .then(() => res.redirect('/')) 
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
@@ -21,10 +21,35 @@ router.get('/:id', (req, res) => {
   return Todo.findOne({
     where: { id, UserId }
   })
-    .then(todo => res.render('detail', { todo: todo }))
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
 })
 
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({
+    where: { id, UserId }
+  })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({
+    where: { id, UserId }
+  })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
 
 
 module.exports = router
