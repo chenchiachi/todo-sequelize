9d44680ae7755aa5d6c372f3cebdade03bff7032
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-01-23 12:51:18 +0100

    Add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,11 @@ const db = require('../../models')
 const Todo = db.Todo
 
 router.get('/', (req, res) => {
+  const userId = req.user.id 
   return Todo.findAll({
     raw: true,
-    nest: true
+    nest: true,
+    where: { userId }
   })
     .then((todos) => { return res.render('index', { todos: todos }) })
     .catch((error) => {
